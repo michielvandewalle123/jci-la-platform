dd33e2bca35a4fa74988911dd7b02da77edd0235
michielvandewalle123 <michiel.vandewalle@gmail.com>
2026-05-31 04:14:25 +0200

    Format response for getData function

--- netlify/functions/getData.mjs
+++ netlify/functions/getData.mjs
@@ -1,7 +1,11 @@
 import { getStore } from "@netlify/blobs";
+
 export default async (req, context) => {
   const store = getStore("jci-data");
   const data = await store.get("db", { type: "json" });
-  return new Response(JSON.stringify(data ?? null), { headers: { "Content-Type": "application/json" } });
+  return new Response(JSON.stringify(data ?? null), {
+    headers: { "Content-Type": "application/json" }
+  });
 };
+
 export const config = { path: "/api/getData" };
